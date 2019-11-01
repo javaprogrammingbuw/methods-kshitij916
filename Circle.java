c39136645a6d4eb11fffb06bf2646bfbfadc85bd
kshitij916 <56730210+kshitij916@users.noreply.github.com>
2019-11-01 16:27:37 +0100

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,5 +1,25 @@
 
 public class Circle {
-	//todo: add some methods to compute a circle's diameter, circumference and area.
-	//print some outputs to confirm your methods are working correctly.
+public class Circle{
+
+int radius;
+
+public static diameter()
+{
+int d = radius*2;
+System.out.println("diameter is:" +d);
+}
+
+public static circumference()
+{
+int c = 2*pi*radius;
+System.out.println("circumference is:" +c);
+}
+
+public static area()
+{
+int a = pi*radius*radius;
+System.out.println("area is:" +a);
+}
+public 
 }
